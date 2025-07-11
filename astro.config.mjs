44ba8d389c75b21bc46d695f04429037c8f3d705
YUI-TIEN <youwei0112@gmail.com>
2025-07-11 11:03:21 +0800

    Update allowedHosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
     },
     preview: {
       host: true,
-      allowedHosts: ['kemeko-morphusai-156eb03f8830.herokuapp.com']
+      allowedHosts: ['kemeko-morphusai-156eb03f8830.herokuapp.com', '.herokuapp.com']
     }
   }
 });
